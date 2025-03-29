d81cb4ae1b6b981486526bb1f246b099fb4fed2c
phihungvo <vophihung987@gmail.com>
2025-03-29 13:21:41 +0700

    build login

--- movienest_app/src/components/Layout/components/Avatar/index.js
+++ movienest_app/src/components/Layout/components/Avatar/index.js
@@ -1,12 +1,20 @@
 import classNames from 'classnames/bind';
 import styles from './Avatar.module.scss';
+import { useNavigate } from 'react-router-dom';
+// import {publicRoute} from 
 
 const cx = classNames.bind(styles);
 
 function Avatar() {
+    const navigate = useNavigate();
+
+    const handleLogin = () => {
+        navigate('/login'); 
+    }
+
     return (
         <div className={cx('avatar-wrapper')}>
-            <div className={cx('container')}>
+            <div className={cx('container')} onClick={() => handleLogin()}>
                 <div className={cx('avatar-container')}>
                     <img src="https://haycafe.vn/wp-content/uploads/2022/10/Hinh-anh-gai-xinh-Viet-Nam-cuoi-tuoi-tan.jpg" alt="Avatar" />
                 </div>
--- movienest_app/src/routes/index.js
+++ movienest_app/src/routes/index.js
@@ -1,9 +1,12 @@
-import Home from "../pages/AdminDashboard/Home";
-import Movie from "../pages/AdminDashboard/Movie";
+import Home from '../pages/AdminDashboard/Home';
+import Movie from '../pages/AdminDashboard/Movie';
+import Login from '../pages/General/Login';
+import Register from '../pages/General/Register';
 
 const publicRoutes = [
     { path: '/', component: Home, title: 'Home' },
-   
+    { path: '/login', component: Login, title: 'Login' },
+    { path: '/register', component: Register, title: 'Register' },
 ];
 
 // const resultsRoutes = [
