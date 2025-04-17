e68c8291de0cbcb3b24db997cf72ef32c0e09808
phihungvo <vophihung987@gmail.com>
2025-04-17 15:31:51 +0700

    Config admin user when start project

--- movienest_api/src/main/java/carevn/luv2code/MovieNest/configuration/ApplicationInitConfig.java
+++ movienest_api/src/main/java/carevn/luv2code/MovieNest/configuration/ApplicationInitConfig.java
@@ -25,21 +25,25 @@ public class ApplicationInitConfig {
 
     @Bean
     ApplicationRunner applicationRunner(UserRepository userRepository){
-        String adminEmail = "admin@admin.com";
+        String adminEmail = "admin@example.com";
         Set<Role> roles = new HashSet<>();
         roles.add(Role.ADMIN);
         return args -> {
             if(userRepository.findByEmail(adminEmail).isEmpty()){
                 User user = User.builder()
-                        .username("admin")
+                        .username("admin1")
                         .email(adminEmail)
-                        .password(passwordEncoder.encode("admin"))
+                        .password(passwordEncoder.encode("admin_example"))
                         .roles(roles)
+                        .enabled(true)
+                        .accountNonExpired(true)
+                        .credentialsNonExpired(true)
+                        .accountNonLocked(true)
                         .build();
 
                 userRepository.save(user);
-                log.warn("Admin user has been created with default email: admin@admin.com" +
-                        " and password: admin, please change it!");
+                log.warn("Admin user has been created with default email: admin@example.com" +
+                        " and password: admin_example, please change it!");
             }
         };
     }
